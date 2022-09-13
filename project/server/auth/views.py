40fa9d7f5068517af16f650af26344aae8cbf1e3
Daniel Delijani <danieldelijani@gmail.com>
2022-09-12 23:06:25 -0400

    fixed encoding/decoding

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -20,7 +20,7 @@ class RegisterAPI(MethodView):
 
     def post(self):
         # get the post data
-        post_data = request.get_json(); print(request)
+        post_data = request.get_json(force=True); print(request)
         # check if user already exists
         user = User.query.filter_by(email=post_data.get('email')).first()
         if not user:
@@ -38,7 +38,7 @@ class RegisterAPI(MethodView):
                 responseObject = {
                     'status': 'success',
                     'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'auth_token': User.decode_auth_token(auth_token)
                 }
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
